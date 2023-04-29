11cfd1b948b1fed49f5750ee9b08a0b35d12ef8b
Emma Metcalfe <emmreese@udel.edu>
2023-04-29 13:19:18 -0400

    Added website name

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from bottle import Bottle, run
 
 app = Bottle()
 
-@app.route('/hello')
+
+@app.route("/henpackhenhack2023")
 def hello():
     return "Hello World!"
 
-run(app, host='localhost', port=8080)
+
+run(app, host="localhost", port=8080)
 # run the program, then go to the link:
-# http://localhost:8080/hello
+# http://localhost:8080/henpackhenhack2023
