10427d1281cb630910e363049545e035f68e90de
Seth Thompson <thompsonrussell22@gmail.com>
2023-04-29 13:19:29 -0400

    added error page

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from bottle import Bottle, run
+from bottle import Bottle, run, error
 
 app = Bottle()
 
 @app.route('/hello')
 def hello():
     return "Hello World!"
+
+@error(404)
+def error404(error):
+    return 'Nothing here, sorry'
 
 run(app, host='localhost', port=8080)
 # run the program, then go to the link:
