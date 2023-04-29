74fa594caef4d190646305f65d51f3b406e1a71e
nicole-kinsella <nkinsell@udel.edu>
2023-04-29 16:49:13 -0400

    Git added welcome.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from nicegui import ui
-
-ui.label('Hello Hens!')
-
-ui.button('Click to Create your Pack!', on_click=lambda: ui.notify(f'You clicked me!'))
-ui.button('Find out what times work best for your pack!', on_click=lambda: ui.notify(f'You clicked me!'))
-
-ui.run()
