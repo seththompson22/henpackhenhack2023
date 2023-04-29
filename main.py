6ec195b25e7ee6e0ceb2165b18dda7389e432b74
jacob wilber <jacobwilber4@gmail.com>
2023-04-29 15:09:37 -0400

    practice button

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from nicegui import ui
 
-ui.label('Hello NiceGUI!')
+ui.label('Hello Hens!')
+
+ui.button('Click to Create your Pack!', on_click=lambda: ui.notify(f'You clicked me!'))
+ui.button('Find out what times work best for your pack!', on_click=lambda: ui.notify(f'You clicked me!'))
 
 ui.run()
