519a793edebddbb5f9287e89e26ef9f2ffcf8b56
Seth Thompson <thompsonrussell22@gmail.com>
2023-04-29 12:34:10 -0400

    Created basic localhost website

---
+++ main.py
@@ -0,0 +1,11 @@
+from bottle import Bottle, run
+
+app = Bottle()
+
+@app.route('/hello')
+def hello():
+    return "Hello World!"
+
+run(app, host='localhost', port=8080)
+# run the program, then go to the link:
+# http://localhost:8080/hello
