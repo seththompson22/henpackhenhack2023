5fca4a2b3620a9fd5e305a43a1173938e00c6c34
Seth Thompson <thompsonrussell22@gmail.com>
2023-04-29 14:22:43 -0400

    changed to use niceGUI

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-from bottle import Bottle, run, error
+from nicegui import ui
 
-app = Bottle()
+ui.label('Hello NiceGUI!')
 
-@app.route('/henpackhenhack2023')
-def hello():
-    return "Hello World!"
-
-@app.error(404)
-def error404(error):
-    return 'Nothing here, sorry'
-
-run(app, host='localhost', port=8080)
-# run the program, then go to the link:
-# http://localhost:8080/henpackhenhack2023
+ui.run()
