959d3d4b7ca9176ad376986c390d79ba51f8fd1a
Seth Thompson <thompsonrussell22@gmail.com>
2023-04-30 08:13:25 -0400

    modified the structure of the javascript files in here

--- structure.js
+++ structure.js
@@ -1,21 +1,112 @@
-import { groupName, emails } from './createPack.html';
-import { start_char, start_hour, start_minute, start_is_morning, end_char, end_hour, end_minute, end_is_morning } from './update_schedule.html';
+ // Define an array to store the emails
+ var emails = [];
 
- class Group {
+ // Function to add an email to the array
+ function addEmail() {
+     var email = document.getElementById("email").value;
+     
+     if (validateEmail(email)) {
+         if (!emails.includes(email)){
+             emails.push(email);
+             document.getElementById("email").value = "";
+             updateGroup();
+         }
+         else {
+             alert("Email already Entered")
+         }
+     } else {
+         alert("Please enter a valid email address.");
+     }
+ }
 
+ // Function to create the group
+ function createGroup() {
+     var groupName = document.getElementById("group-name").value;
+     if (groupName === "") {
+         alert("Please enter a group name.");
+     } else if (emails.length === 0) {
+         alert("Please add at least one email to the group.");
+     } else {
+         var groupList = document.createElement("ul");
+         groupList.setAttribute('id','email-list');
+         for (var i = 0; i < emails.length; i++) {
+             var emailListItem = document.createElement("li");
+             emailListItem.textContent = emails[i];
+             emailListItem.setAttribute('id','emailListItem');
+             groupList.appendChild(emailListItem);
+         }
+         document.getElementById("group").innerHTML = "";
+         document.getElementById("group").appendChild(groupList);
+         alert("Group \"" + groupName + "\" created with " + emails.length + " members.");
+     }
+ }
+
+ // Function to validate an email address
+ function validateEmail(email) {
+     var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+     return regex.test(email);
+ }
+
+ // Function to update the group display
+ function updateGroup() {
+     var groupList = document.createElement("ul");
+     var uniqueEmails = [];
+
+     var emailContainer = document.getElementById("containerB");
+
+     // Remove previous ul element
+     var previousList = document.getElementById("email-list");
+     if (previousList) {
+         previousList.remove();
+     }
+
+     for (var j = 0; j < emails.length; j++) {
+         if (!uniqueEmails.includes(emails[j])) {
+             uniqueEmails.push(emails[j]);
+             var emailListItem = document.createElement("li");
+             emailListItem.textContent = emails[j];
+             emailListItem.setAttribute('id', 'emailListItem');
+             groupList.appendChild(emailListItem);
+         } else {
+             groupList.removeChild(document.getElementById("emailListItem"));
+         }
+     }
+
+     groupList.setAttribute('id', 'email-list');
+     emailContainer.appendChild(groupList);
+ }
+ 
+
+ function directGroup(emails){
+     window.location.href = "https://seththompson22.github.io/henpackhenhack2023/update_schedule.html";
+ }
+
+ function updateTime() {
+    var start_char = document.getElementById("select-start-day");
+    var start_hour = document.getElementById("start_hour");
+    var start_minute = document.getElementById("start-minute");
+    var start_is_morning = document.getElementById("select-AM-PM");
+    var end_char = document.getElementById("select-end-day");
+    var end_hour = document.getElementById("end_hout");
+    var end_minute = document.getElementById("end-minute");
+    var end_is_morning = document.getElementById("select-AM-PM2");
+}
+
+
+class Group {
     constructor(name, emails) {
-      this.name = name;
-      this.emails = emails;
+        this.name = name;
+        this.emails = emails;
     }
  }
- class Event {
+class Event {
     constructor(){
         this.start = start;
         this.end = end;
     }
  }
 
- class TimeData {
+class TimeData {
     constructor(day_char, hour, minute, is_morning){
         this.day_char = day_char;
         this.hour = hour;
@@ -34,7 +125,7 @@ const friday_availability = new Array(new Event(TimeData('F',0,0,true), TimeData
 const saturday_availability = new Array(new Event(TimeData('S',0,0,true), TimeData('S',23,59,false)));
 const sunday_availability = new Array(new Event(TimeData('U',0,0,true), TimeData('U',23,59,false)));
 weekly_available = [monday_availability, tuesday_availability, wednesday_availability, thursday_availability, friday_availability, saturday_availability, sunday_availability];
-group = new Group("GroupName", {1:"thompson@gmail.com", 2:"srt@udel.edu", 3:"emerson@gmail.com"}, weekly_available);
+group = new Group(groupName, emails, weekly_available);
 
 
 // get user input and store it as an event
